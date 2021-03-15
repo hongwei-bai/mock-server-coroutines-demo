b86d6aa4a29095074ef416c8d66155ccd7b2d4a1
Hongwei <bhw8412@hotmail.com>
2021-03-16 07:29:36 +1100

    add support war deployment

--- src/main/java/com/hongwei/DemoApplication.java
+++ src/main/java/com/hongwei/DemoApplication.java
@@ -2,12 +2,31 @@ package com.hongwei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+//@SpringBootApplication
+//@EnableScheduling
+//public class DemoApplication {
+//    public static void main(String[] args) {
+//        SpringApplication.run(DemoApplication.class, args);
+//    }
+//}
+
+// Deploy 3 - deploy war to Tomcat/NGINX server
+@EntityScan
 @SpringBootApplication
 @EnableScheduling
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
     }
 }
--- src/main/java/com/hongwei/controller/DemoController.kt
+++ src/main/java/com/hongwei/controller/DemoController.kt
@@ -7,11 +7,13 @@ import com.hongwei.service.MockContentService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
 
 @Controller
+@CrossOrigin
 class DemoController {
     companion object {
         private const val ContentApiDelay = 7000L
